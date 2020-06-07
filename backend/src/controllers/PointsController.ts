889c6af1954ffe5df73bbf986866238e758ac7d4
Bruno Barbosa <barbosa.developer@gmail.com>
2020-06-07 06:10:12 -0300

    Upload de imagens e validação de campos

--- backend/src/controllers/PointsController.ts
+++ backend/src/controllers/PointsController.ts
@@ -9,18 +9,20 @@ class PointsController {
 
     const point = {
       name, email, whatsapp, latitude, longitude, city, uf,
-      image: 'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+      image: request.file.filename,
     };
 
     const insertedIds = await trx('points').insert(point);
 
     const point_id = insertedIds[0];
 
-    const pointItems = items.map((item_id: number) => {
-      return {
-        item_id,
-        point_id: point_id,
-      }
+    const pointItems = items.split(',')
+      .map((item: string) => Number(item.trim()))
+      .map((item_id: number) => {
+        return {
+          item_id,
+          point_id: point_id,
+        }
     })
     await trx('point_items').insert(pointItems);
 
@@ -41,12 +43,17 @@ class PointsController {
       return response.status(400).json({message: 'Point not found.'});
     }
 
+    const serializedPoint = {
+      ...point,
+      image_url: `http://192.168.11.101:3333/uploads/${point.image}`,
+    };
+
     const items = await knex('items')
       .join('point_items', 'items.id', '=', 'point_items.item_id')
       .where('point_items.point_id', id)
       .select('items.title');
 
-    return response.json({point, items});
+    return response.json({point: serializedPoint, items});
   }
 
   async index(request: Request, response: Response) {
@@ -63,8 +70,15 @@ class PointsController {
       .where('uf', String(uf))
       .distinct()
       .select('points.*');
+
+    const serializedPoints = points.map(point => {
+      return {
+        ...point,
+        image_url: `http://192.168.11.101:3333/uploads/${point.image}`,
+      }
+    })
     
-    return response.json(points);
+    return response.json(serializedPoints);
   }
 }
 
